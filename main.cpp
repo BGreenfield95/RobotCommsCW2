55b0cc81ef982b53c38639294fc79d417934d723
Brett <brettgreenfield@hotmail.co.uk>
2024-04-11 14:49:37 +0100

    Add Zigbee.h & Zigbee.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "mbed.h"
+#include <cstdint>
+#include <cstring>
+
+PwmOut servo(PB_3);
 
 // main() runs in its own thread in the OS
 int main()
 {
-    while (true) {
-
-    }
+    servo.period(0.020);          // servo requires a 20ms period
+    while (1) {
+        for(float offset=0.0; offset<0.001; offset+=0.0001) {
+            servo.pulsewidth(0.001 + offset); // servo position determined by a pulsewidth between 1-2ms
+            ThisThread::sleep_for(chrono::seconds(1));;
+        }
 }
 
-//Test One v1
