32035eadcecf485659a228d341f1086d59e7ab4b
Brett <brettgreenfield@hotmail.co.uk>
2024-04-13 21:24:33 +0100

    Maybe fixed?

--- main.cpp
+++ main.cpp
@@ -14,11 +14,9 @@
 #define BUFFSIZE    64
 
 BufferedSerial pcServo(USBTX, USBRX, 115200);
-Zigbee ZigbeeServo(PA_2, PA_3); 
+Zigbee ZigbeeServo(PA_9, PA_10); 
 
 PwmOut servo(PB_3);
-
-
 
 char buffer[BUFFSIZE]   = {0};
 char msgBuff[BUFFSIZE]  = {0};
@@ -31,50 +29,49 @@ Mutex serialMutex;
 
 void reader()
 {
-    servo.period_ms(20);
-    servo.pulsewidth_us(MID); //NB in microseconds
-    
     while(1)
     {
-        printf("1");
         if(ZigbeeServo.receiveMessage(rcvBuff))
         {
-            printf("2");
             if(serialMutex.trylock_for(chrono::milliseconds(500)))
             {
-                printf("ZibbeeServo");
-                printf("ZigbeeServo, received: %s", rcvBuff);
+                printf("Received");
+                len = snprintf(msgBuff, BUFFSIZE, "\r\n%s", rcvBuff);
+                pcServo.write(msgBuff, len);
+                serialMutex.unlock();
 
                 if(strcmp(rcvBuff, "0") == 0){
                     servo.pulsewidth_us(MIN);
-                    printf("Name: ZigbeeServo\r\n");
-                    len = snprintf(msgBuff, BUFFSIZE, "\r\n%s", rcvBuff);
-                    pcServo.write(msgBuff, len);
-                    serialMutex.unlock();
                 }
 
                 if(strcmp(rcvBuff, "1") == 0){
                     servo.pulsewidth_us(MAX);
-                    len = snprintf(msgBuff, BUFFSIZE, "\r\n%s", rcvBuff);
-                    pcServo.write(msgBuff, len);
-                    serialMutex.unlock();
                 }
-
             }
-        }
         ThisThread::sleep_for(chrono::milliseconds(10));
+        }
     }
 }
 
 int main()
 {
+    servo.period_ms(20);
+    servo.pulsewidth_us(MID); //NB in microseconds
+    
     pcServo.set_format(
         /* bits */ 8,
         /* parity */ BufferedSerial::None,
         /* stop bit */ 1
     );
     readThread.start(reader);
-
+    
+    if(serialMutex.trylock_for(chrono::milliseconds(500)))
+    {
+        printf("Started");
+        len = snprintf(buffer, BUFFSIZE, "\r\nHello, I have started :)\r\n");
+        pcServo.write(buffer, len);
+        serialMutex.unlock();
+    }
 }
 
 /*
