f7b9219e91360b6cc66097f509afee85825e1362
Brett <brettgreenfield@hotmail.co.uk>
2024-04-14 19:38:56 +0100

    Edit px.write

--- main.cpp
+++ main.cpp
@@ -61,7 +61,7 @@ int main()
     }
 
     while (true) {
-        len = snprintf(msgBuff, BUFFSIZE, "%d\n", LDR.read_u16());
+        len = snprintf(msgBuff, BUFFSIZE, "%d\r\n", LDR.read_u16());
         pc.write(msgBuff, len);
         xbee.sendMessage(msgBuff);
         counter++;
